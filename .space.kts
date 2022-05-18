05112878ff7f6d9070e2953f45d076fabf818354
Zhashtilek <zhashtilek.orozov@mavix.io>
2022-05-18 12:33:50 +0600

    ci: add dockerfile, .space.kts

---
+++ .space.kts
@@ -0,0 +1,24 @@
+job("Build and push Docker") {
+  startOn {
+    gitPush {
+      // run on changes in all 'release-...'
+      branchFilter {
+        +"refs/heads/release-*"
+      }
+    }
+  }
+  docker {
+    beforeBuildScript {
+      content = """
+          export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'-' -f 2)
+      """
+    }
+    build {
+      file = "./Dockerfile"
+    }
+
+    push("fixturr.registry.jetbrains.space/p/software-development/registry/core") {
+      tags("V\$BRANCH")
+    }
+  }
+}
