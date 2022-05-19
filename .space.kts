451a8c66bea6df5bc07e6235b69ea0720a15b8d2
Zhashtilek <zhashtilek.orozov@mavix.io>
2022-05-19 12:24:20 +0600

    ci: add deploy step

--- .space.kts
+++ .space.kts
@@ -1,9 +1,9 @@
 job("Build and push Docker") {
   startOn {
     gitPush {
-      // run on changes in all 'release-...'
+      // run on changes in all 'build-...'
       branchFilter {
-        +"refs/heads/release-*"
+        +"refs/heads/build-*"
       }
     }
   }
@@ -19,6 +19,31 @@ job("Build and push Docker") {
 
     push("fixturr.registry.jetbrains.space/p/software-development/registry/ui") {
       tags("V\$BRANCH")
+    }
+  }
+}
+
+job("Deploy to dev") {
+  requirements {
+    workerPool = WorkerPools.SELF_HOSTED
+  }
+  startOn {
+    gitPush {
+      branchFilter {
+        +"refs/heads/release-*"
+      }
+    }
+  }
+  host("Run script") {
+    shellScript {
+      interpreter = "/bin/bash"
+      content = """
+        export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'-' -f 2)
+        && cd /home/runner/fixturr-compose/ui
+        && export TAG=V$BRANCH && echo \"$TAG\" && docker-compose pull ui
+        && docker-compose up -d --force-recreate ui
+        && echo 'Container has been started'
+      """
     }
   }
 }
