6afaba6cc8fc53149af237ac6a3d28b7be8277a2
Zhashtilek <zhashtilek.orozov@mavix.io>
2022-05-19 13:14:09 +0600

    ci: fix variables

--- .space.kts
+++ .space.kts
@@ -39,10 +39,10 @@ job("Deploy to dev") {
       interpreter = "/bin/bash"
       content = """
         export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'-' -f 2)
-        && cd /home/runner/fixturr-compose/ui
-        && export TAG=V\$BRANCH && echo \$TAG && docker-compose pull ui
-        && docker-compose up -d --force-recreate ui
-        && echo 'Container has been started'
+        cd /home/runner/fixturr-compose/ui
+        export TAG=V${'$'}BRANCH && echo ${'$'}TAG && docker-compose pull ui
+        docker-compose up -d --force-recreate ui
+        echo 'Container has been started'
       """
     }
   }
