65e57bb9937d209cc9c0dd1c689e1f3576ea396c
Samat Jorobekov <samat.jorobekov@gmail.com>
2022-05-20 16:22:10 +0600

    #1: superadmin - sports

--- components/FxProfilePictureForm.vue
+++ components/FxProfilePictureForm.vue
@@ -1,16 +1,21 @@
 <template>
   <v-form ref="form" v-async-form :disabled="disabled" lazy-validation>
-    <v-row v-if="!file" class="fill-height" align="center">
+    <v-row class="fill-height" align="center">
       <v-col cols="12">
         <FxAvatar class="mr-2" size="80" />
         <v-file-input
           ref="file"
-          v-model="file"
           style="display: none"
           accept="image/*"
           @change="uploadImage"
         />
-        <v-btn depressed color="primary" class="mr-2" @click="reselect">
+        <v-btn
+          depressed
+          color="primary"
+          class="mr-2"
+          :loading="loading"
+          @click="reselect"
+        >
           <v-icon>$vuetify.icons.upload</v-icon>Upload
         </v-btn>
         <v-btn depressed outlined>
@@ -18,41 +23,17 @@
         </v-btn>
       </v-col>
     </v-row>
-
-    <v-row v-else>
-      <v-col cols="6" sm="8">
-        <cropper
-          ref="cropper"
-          class="custom-restrictions-cropper"
-          :src="image"
-          :stencil-size="stencilSize"
-          :stencil-props="stencilProps"
-          image-restriction="stencil"
-          :min-height="limitations.minHeight"
-          :min-width="limitations.minWidth"
-          stencil-component="circle-stencil"
-        />
-      </v-col>
-      <v-col cols="6" sm="8" class="text-right">
-        <v-btn outlined @click="clear">
-          Cancel
-        </v-btn>
-        <v-btn dark depressed color="brand2" @click="save">
-          Save
-        </v-btn>
-      </v-col>
-    </v-row>
+    <FxImageCropModal ref="cropper" />
   </v-form>
 </template>
 
 <script>
-import { Cropper } from 'vue-advanced-cropper'
 import FxAvatar from '@/components/FxAvatar'
 import 'vue-advanced-cropper/dist/style.css'
 
 export default {
   name: 'AccountStatusForm',
-  components: { FxAvatar, Cropper },
+  components: { FxAvatar },
   props: {
     value: {
       type: Object,
@@ -62,48 +43,36 @@ export default {
       type: Boolean,
       default: false,
     },
+    loading: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   data: () => ({
     file: null,
     image: null,
-    stencilSize: {
-      width: 300,
-      height: 300,
-    },
-    limitations: {
-      minWidth: 300,
-      minHeight: 300,
-    },
-    stencilProps: {
-      handlers: {},
-      movable: false,
-      resizable: false,
-      aspectRatio: 1,
+    preview: null,
+    compression: {
+      maxSizeMB: 0.5,
+      maxWidthOrHeight: 300,
     },
   }),
 
   methods: {
-    _crop () {
-      return new Promise((resolve) => {
-        const { canvas } = this.$refs.cropper.getResult()
-        canvas.toBlob((blob) => {
-          resolve(blob)
-        }, 'image/webp')
-      })
-    },
+    async uploadImage (file) {
+      if (!file) {
+        return
+      }
 
-    uploadImage () {
-      if (this.file) {
-        const reader = new FileReader()
-        // Define a callback function to run, when FileReader finishes its job
-        reader.onload = (e) => {
-          // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
-          // Read image as base64 and set to imageData
-          this.image = e.target.result
-        }
-        // Start the reader job - read file as a data url (base64 format)
-        reader.readAsDataURL(this.file)
+      const base64Image = await this.readBlob(file)
+
+      const imageBlob = await this.$refs.cropper.open(base64Image)
+      if (imageBlob) {
+        this.$emit('input', {
+          ...this.value,
+          file: imageBlob,
+        })
       }
     },
 
@@ -119,9 +88,14 @@ export default {
       this.image = null
     },
 
-    async save () {
-      const result = await this._crop()
-      this.$emit('input', result)
+    readBlob (blob) {
+      return new Promise((resolve) => {
+        const reader = new FileReader()
+        reader.onload = (e) => {
+          resolve(e.target.result)
+        }
+        reader.readAsDataURL(blob)
+      })
     },
   },
 }
