0bf060eca238c7bef2ab362f3c38e0d7e7882e13
Zhashtilek <zhashtilek.orozov@mavix.io>
2022-05-20 16:49:19 +0600

    #1: trigger jobs manually

--- .space.kts
+++ .space.kts
@@ -1,12 +1,4 @@
 job("Build and push Docker") {
-  startOn {
-    gitPush {
-      // run on changes in all 'build-...'
-      branchFilter {
-        +"refs/heads/build-*"
-      }
-    }
-  }
   docker {
     beforeBuildScript {
       content = """
@@ -18,7 +10,7 @@ job("Build and push Docker") {
     }
 
     push("fixturr.registry.jetbrains.space/p/software-development/registry/ui") {
-      tags("V\$BRANCH")
+      tags("\$BRANCH")
     }
   }
 }
@@ -27,20 +19,13 @@ job("Deploy to dev") {
   requirements {
     workerPool = WorkerPools.SELF_HOSTED
   }
-  startOn {
-    gitPush {
-      branchFilter {
-        +"refs/heads/release-*"
-      }
-    }
-  }
   host("Run script") {
     shellScript {
       interpreter = "/bin/bash"
       content = """
-        export BRANCH=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'-' -f 2)
+        export TAG=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'-' -f 2)
         cd /home/runner/fixturr-compose/ui
-        export TAG=V${'$'}BRANCH && echo ${'$'}TAG && docker compose pull ui
+        echo ${'$'}TAG && docker compose pull ui
         docker compose up -d --force-recreate ui
         echo 'Container has been started'
       """
