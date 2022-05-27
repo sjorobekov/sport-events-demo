0cd0ced748189082da1311bf32230d61d6a193a1
Samat Jorobekov <samat@mavix.io>
2022-05-27 16:55:46 +0600

    feat(RC20220527): regression fixes

--- store/index.js
+++ store/index.js
@@ -1,11 +1,15 @@
 export const actions = {
   async nuxtServerInit ({ dispatch }) {
-    try {
-      await dispatch('context/fetchContext')
-      await dispatch('seasons/fetch')
-    } catch (e) {
+    await dispatch('context/fetchContext').catch((e) => {
+      // eslint-disable-next-line no-console
+      if (e.response?.status !== 401) {
+        console.log('error', e)
+      }
+    })
+
+    await dispatch('seasons/fetch').catch((e) => {
       // eslint-disable-next-line no-console
       console.log('error', e)
-    }
+    })
   },
 }
