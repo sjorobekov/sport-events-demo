e8078ee4ee0e5f0dfa652826d6e6dafaca7c741b
Samat Jorobekov <samat@mavix.io>
2022-08-10 16:47:04 +0600

    fix(SOFTWARE-DEVELOPMENT-T-142): fix design of result radiobuttons

--- components/FxEventResultForm/components/FxResultRadio.vue
+++ components/FxEventResultForm/components/FxResultRadio.vue
@@ -2,13 +2,13 @@
   <v-radio-group class="mt-0 pt-0" hide-details :value="value" @change="$emit('input', $event)">
     <v-row>
       <v-col cols="4">
-        <v-radio on-icon="mdi-check-circle" color="success" class="radio rounded-l rounded-r px-2" :value="EventResult.WIN" label="Win" />
+        <v-radio on-icon="mdi-check-circle" color="success" class="win radio rounded-l rounded-r px-2" :value="EventResult.WIN" label="Win" />
       </v-col>
       <v-col cols="4">
-        <v-radio on-icon="mdi-check-circle" color="success" class="radio rounded-l rounded-r py-4 px-2" :value="EventResult.DRAW" label="Draw" />
+        <v-radio on-icon="mdi-check-circle" color="info lighten-2" class="draw radio rounded-l rounded-r py-4 px-2" :value="EventResult.DRAW" label="Draw" />
       </v-col>
       <v-col cols="4">
-        <v-radio on-icon="mdi-check-circle" color="success" class="radio rounded-l rounded-r py-4 px-2" :value="EventResult.LOST" label="Loss" />
+        <v-radio on-icon="mdi-check-circle" color="error" class="lost radio rounded-l rounded-r py-4 px-2" :value="EventResult.LOST" label="Loss" />
       </v-col>
     </v-row>
   </v-radio-group>
@@ -31,13 +31,21 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .radio {
   height: 52px;
   box-shadow: 0 0 0 1px var(--v-info-lighten2);
 }
 
 .radio.v-item--active {
-  box-shadow: 0 0 0 1px var(--v-success-base);
+  &.win {
+    box-shadow: 0 0 0 2px var(--v-success-base);
+  }
+  &.draw {
+    box-shadow: 0 0 0 2px var(--v-info-lighten1);
+  }
+  &.lost {
+    box-shadow: 0 0 0 2px var(--v-error-base);
+  }
 }
 </style>
