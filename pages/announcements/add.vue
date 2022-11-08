312508626c608578e4e7dfccf96ed6989cf1a736
Ruslan Ismailov <ruslan.ismailov@mavix.io>
2022-11-08 14:36:17 +0600

    fix(SOFTWARE-DEVELOPMENT-T-237): make redirect to announcements page

--- pages/announcements/add.vue
+++ pages/announcements/add.vue
@@ -65,7 +65,7 @@ export default {
         ...this.formData,
       }).then((res) => {
         this.$emit('created', res)
-        this.$router.push({ name: 'announcements', query: { id: res.id } })
+        $router.push({ name: 'announcements', query: { id: res.id } })
       }).finally(() => {
         this.loading = false
       })
