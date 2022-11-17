05aa24513320ab8dfe2fd67a296a123766c3b0ca
Alex Shabalin <alex.shabalin@mavix.io>
2022-11-17 16:18:35 +0600

    fix(SOFTWARE-DEVELOPMENT-T-243): add map to event page

---
+++ components/FxMap.vue
@@ -0,0 +1,17 @@
+<template>
+  <FxEventItemCard>
+    <template #title>
+      Map
+    </template>
+    <FxMapCard />
+  </FxEventItemCard>
+</template>
+
+<script>
+import FxEventItemCard from '@/components/PageComponents/FxEventIndividualPage/FxEventItemCard'
+import FxMapCard from '@/PageComponents/FxOpponentPage/FxMapCard'
+export default {
+  name: 'FxMap',
+  components: { FxEventItemCard, FxMapCard },
+}
+</script>
