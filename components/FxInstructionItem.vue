26d9964882c023981991639768487f0174b8a345
Zhashtilek <zhashtilek.orozov@mavix.io>
2022-11-22 21:28:44 +0600

    feat(SOFTWARE-DEVELOPMENT-T-315): change instruction item height, width

--- components/FxInstructionItem.vue
+++ components/FxInstructionItem.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-list class="my-0">
+  <v-list dense class="my-0">
     <v-list-item class="my-0">
-      <v-list-item-avatar class="my-0">
+      <v-list-item-avatar class="my-0" height="32" min-width="32" width="32">
         <v-avatar color="primary">
           <span class="white--text text-h5">
             <slot name="number" />
@@ -17,7 +17,7 @@
     </v-list-item>
 
     <v-list-item class="my-0">
-      <v-list-item-avatar class="py-0 my-0" />
+      <v-list-item-avatar class="py-0 my-0" height="32" min-width="32" width="32" />
       <v-list-item-content class="py-0 text-p1 info--text text--darken-1">
         <slot name="content" />
       </v-list-item-content>
@@ -36,5 +36,8 @@ export default {
   font-size: rem(18px)!important;
   color: var(--v-info-darken3)!important;
   font-weight: 600;
+}
+.v-list--dense .v-list-item {
+  min-height: 32px;
 }
 </style>
