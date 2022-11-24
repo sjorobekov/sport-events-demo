1ca80efe837f2dde27ad6970d450c5c38b40d32c
Samat Jorobekov <samat@mavix.io>
2022-11-24 13:54:29 +0600

    feat(SOFTWARE-DEVELOPMENT-T-278): center items in calendar and team pages

--- components/FxEventItem/ListItem.vue
+++ components/FxEventItem/ListItem.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-list-item class="px-0">
-    <v-list-item-icon class="mr-3">
-      <slot name="icon" />
-    </v-list-item-icon>
-    <v-list-item-content>
-      <slot name="content">
-        <v-list-item-title class="text-p2 info--text text--darken-4">
-          <slot name="title" />
-        </v-list-item-title>
-        <v-list-item-subtitle class="text-p1 info--text">
-          <slot name="subtitle" />
-        </v-list-item-subtitle>
-      </slot>
-    </v-list-item-content>
-  </v-list-item>
+  <div>
+    <v-list-item class="px-0 d-inline-flex">
+      <v-list-item-icon class="mr-3 flex-grow-0 flex-shrink-1">
+        <slot name="icon" />
+      </v-list-item-icon>
+      <v-list-item-content>
+        <slot name="content">
+          <v-list-item-title class="text-p2 info--text text--darken-4">
+            <slot name="title" />
+          </v-list-item-title>
+          <v-list-item-subtitle class="text-p1 info--text">
+            <slot name="subtitle" />
+          </v-list-item-subtitle>
+        </slot>
+      </v-list-item-content>
+    </v-list-item>
+  </div>
 </template>
 
 <script>
