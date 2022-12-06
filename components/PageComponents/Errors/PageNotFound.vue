3469b2de460dfa1822ae030e7b7b1f998479fac9
Samat Jorobekov <samat@mavix.io>
2022-12-06 16:24:57 +0600

    feat(SOFTWARE-DEVELOPMENT-T-266): page not found design

---
+++ components/PageComponents/Errors/PageNotFound.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="error-container text-center d-flex justify-center align-center">
+    <div>
+      <h1 class="error-title mb-8">
+        Oops, <br><span class="primary--text">nothing</span> here...
+      </h1>
+      <p class="subtitle mb-2 info--text text--darken-1">
+        Sorry, the page you're looking for can't be found.
+      </p>
+      <p class="subtitle mb-8 info--text text--darken-1">
+        Try going back to the home page.
+      </p>
+
+      <v-btn depressed color="primary" class="text-center" link :to="{ name: 'home' }">
+        <v-icon>mdi-arrow-right</v-icon> Go to Sports Home
+      </v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PageNotFound',
+}
+</script>
+
+<style scoped>
+.error-container {
+  max-width: 600px;
+  margin: 0 auto;
+  height: 90vh
+}
+.error-title {
+  font-size: 5rem;
+  line-height: 5rem;
+}
+.subtitle {
+  font-size: 18px;
+  line-height: 24px;
+}
+</style>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,21 +1,32 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <div>
+    <component :is="item.component" :error="error" />
+  </div>
 </template>
 
 <script>
+import PageNotFound from '@/components/PageComponents/Errors/PageNotFound'
+import UnknownError from '@/components/PageComponents/Errors/UnknownError'
+
 export default {
   name: 'EmptyLayout',
-  layout: 'empty',
+  components: {
+    PageNotFound,
+    UnknownError,
+  },
+  layout ({ store }) {
+    if (store.getters['context/isPortalSite']) {
+      return 'default'
+    }
+
+    if (store.getters['context/isSuperAdminSite']) {
+      return 'admin'
+    }
+
+    if (store.getters['context/isSignUpSite']) {
+      return 'startup'
+    }
+  },
   props: {
     error: {
       type: Object,
@@ -26,20 +37,30 @@ export default {
     return {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred',
+      pages: {
+        404: {
+          title: 'Oops, page can\'t be found',
+          component: 'PageNotFound',
+        },
+        unknown: {
+          title: 'Unknown Error',
+          component: 'UnknownError',
+        },
+      },
     }
   },
+
   head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+    const title = this.item.title
     return {
       title,
     }
   },
+
+  computed: {
+    item () {
+      return this.pages[this.error.statusCode] || this.pages.unknown
+    },
+  },
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
