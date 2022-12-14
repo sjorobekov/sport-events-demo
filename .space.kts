69f3400328a90ad51fc7aa90317ca1b6ef25d424
Samat Jorobekov <samat@mavix.io>
2022-12-14 16:10:38 +0600

    feat(SOFTWARE-DEVELOPMENT-T-348): ci for prod

--- .space.kts
+++ .space.kts
@@ -18,6 +18,7 @@ job("Build and push Docker") {
 job("Deploy to dev") {
   requirements {
     workerPool = WorkerPools.SELF_HOSTED
+    workerTags("dev")
   }
   host("Run script") {
     shellScript {
@@ -25,6 +26,26 @@ job("Deploy to dev") {
       content = """
         export TAG=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
         cd /home/runner/fixturr-compose/ui
+        echo ${'$'}TAG && docker compose pull ui
+        docker compose up -d --force-recreate ui
+        echo 'Container has been started'
+      """
+    }
+  }
+}
+
+
+job("Deploy to prod") {
+  requirements {
+    workerPool = WorkerPools.SELF_HOSTED
+    workerTags("prod")
+  }
+  host("Deploying Docker Image") {
+    shellScript {
+      interpreter = "/bin/bash"
+      content = """
+        export TAG=${'$'}(echo ${'$'}JB_SPACE_GIT_BRANCH | cut -d'/' -f 3)
+        cd /home/fixturr/app/ui
         echo ${'$'}TAG && docker compose pull ui
         docker compose up -d --force-recreate ui
         echo 'Container has been started'
