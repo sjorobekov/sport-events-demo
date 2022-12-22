273e0fde13d4e9d0d7476ee5019bc8cd0ba58d5d
Alex Shabalin <alex.shabalin@mavix.io>
2022-12-22 15:45:06 +0600

    feat(SOFTWARE-DEVELOPMENT-T-409): style request sent page

--- components/PageComponents/FxSignUpPage/RequestSent.vue
+++ components/PageComponents/FxSignUpPage/RequestSent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="custom-width">
     <h1 class="text-h1 primary--text text-center mt-6">
       Request Sent
     </h1>
@@ -11,7 +11,8 @@
     <p class="text-p2 info--text text-center">
       The admin for {{ school.name }} has been sent a request to create an account for you.
       <br>
-      If no one at your school has signed up for Fixturr, or there is a problem with this request, please contact us at help@fixturr.com
+      <br>
+      If no one at your school has signed up for Fixturr, or there is a problem with this request, please contact us at <a href="mailto: help@fixturr.com"> help@fixturr.com</a>
     </p>
   </div>
 </template>
@@ -27,3 +28,12 @@ export default {
   },
 }
 </script>
+
+<style>
+.custom-width {
+  width:100%;
+  max-width: 640px;
+  margin: 0px auto;
+}
+
+</style>
