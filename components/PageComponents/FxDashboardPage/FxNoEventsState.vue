566217fb01eb14d1d9720d93d5569d28b6785599
Alex Shabalin <alex.shabalin@mavix.io>
2022-12-28 16:38:21 +0600

    feat(SOFTWARE-DEVELOPMENT-T-365): change button font and remove second title

--- components/PageComponents/FxDashboardPage/FxNoEventsState.vue
+++ components/PageComponents/FxDashboardPage/FxNoEventsState.vue
@@ -3,18 +3,14 @@
     <v-row>
       <v-col class="d-flex flex-column justify-center align-center">
         <v-img width="100" :src="calendar" class="mb-3"/>
-        <div class="text-p3 text-center info--text text--darken-3 mb-2">
+        <div class="text-p3 text-center info--text text--darken-3 mb-4">
           No Events Today
         </div>
-        <div class="text-p2 text-center info--text text--darken-3 mb-4" style="width:130px">
-          Try to change the date or
-        </div>
-
         <v-btn
           link
-          color="info darken-3"
           outlined
           :to="{ name: 'calendar' }"
+          class="text-p1 font-weight-bold info--text text--darken-1"
         >
           Go to Calendar
         </v-btn>
