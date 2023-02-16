b26c5c9cc42348c07a8d756387a2f15dfaf0f121
Alex Shabalin <alex.shabalin@mavix.io>
2023-02-16 16:53:00 +0600

    fix(SOFTWARE-DEVELOPMENT-T-404): make router to teamID

--- components/PageComponents/FxEventIndividualPage/FxEventIndividualPage.vue
+++ components/PageComponents/FxEventIndividualPage/FxEventIndividualPage.vue
@@ -126,7 +126,7 @@ export default {
         id: this.event.id,
       }).then(() => {
         this.$toast('Event has been removed')
-        this.$router.push({ name: 'teams' })
+        this.$router.push({ name: 'teams', params: { schoolId: this.teamId } })
       }).catch(() => {
         this.$toast.error('Unknown Error')
       })
