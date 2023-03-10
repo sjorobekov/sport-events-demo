03bd519d5659f5caf8cc91bbe7ef09c2e22a15f6
Alex Shabalin <alex.shabalin@mavix.io>
2023-03-10 15:00:50 +0600

    fix(SOFTWARE-DEVELOPMENT-T-374): update condition

--- pages/teams/_id/index.vue
+++ pages/teams/_id/index.vue
@@ -122,7 +122,7 @@
             </template>
           </v-col>
         </v-row>
-        <div v-else-if="!$fetchState.pending && !upcoming.length || !past.length">
+        <div v-else-if="!$fetchState.pending && !upcoming.length && !past.length">
           <v-col class="d-flex flex-column justify-center align-center pt-12">
             <v-img width="100" :src="noEvents" class="mb-3" />
             <div class="text-p3 text-center info--text text--darken-3 mb-4">
