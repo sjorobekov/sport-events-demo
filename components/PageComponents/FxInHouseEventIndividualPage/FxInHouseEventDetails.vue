2e56b8ab2c782988063b58dc07adcbb0ae82e7d6
Zhashtilek <zhashtilek.orozov@mavix.io>
2023-04-12 11:24:23 +0600

    feat(SOFTWARE-DEVELOPMENT-T-637): add edit buttons on in house event page for mobile

--- components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventDetails.vue
+++ components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventDetails.vue
@@ -1,108 +1,122 @@
 <template>
-  <FxEventItemCard>
-    <template #title>
-      Details
-    </template>
-    <template #actions>
-      <v-btn v-if="canEditEvent && !formVisible" outlined @click="formVisible = true">
-        <v-icon small>
-          mdi-pencil-outline
-        </v-icon>Edit Details
-      </v-btn>
-    </template>
-    <FxInHouseEventDetailsForm
-      v-if="formVisible"
-      ref="form"
-      v-model="formData"
-      class="px-6"
-      :disabled="loading"
-      @submit="save"
-      @cancel="formVisible = false"
-    />
-    <v-container v-else>
-      <v-row>
-        <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
-          <ListItem>
-            <template #icon>
-              <v-icon>$vuetify.icons.calendar-1</v-icon>
-            </template>
-            <template #title>
-              <FxDateFormat :date="match.date" />
-            </template>
-            <template #subtitle>
-              Date
-            </template>
-          </ListItem>
-        </v-col>
-        <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
-          <ListItem>
-            <template #icon>
-              <v-icon>$vuetify.icons.clock-1</v-icon>
-            </template>
-            <template #content>
-              <div>
-                <div class="d-inline-block pr-8">
-                  <v-list-item-title class="text-p2 neutral--text text--darken-4">
-                    {{ match.startTime }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle class="text-p1 neutral--text text--darken-2">
-                    Start Time
-                  </v-list-item-subtitle>
+  <div>
+    <FxEventItemCard>
+      <template #title>
+        Details
+      </template>
+      <template #actions>
+        <v-btn v-if="canEditEvent && !formVisible" outlined @click="formVisible = true">
+          <v-icon small>
+            mdi-pencil-outline
+          </v-icon>Edit Details
+        </v-btn>
+      </template>
+      <FxInHouseEventDetailsForm
+        v-if="formVisible"
+        ref="form"
+        v-model="formData"
+        class="px-6"
+        :disabled="loading"
+        @submit="save"
+        @cancel="formVisible = false"
+      />
+      <v-container v-else>
+        <v-row>
+          <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
+            <ListItem>
+              <template #icon>
+                <v-icon>$vuetify.icons.calendar-1</v-icon>
+              </template>
+              <template #title>
+                <FxDateFormat :date="match.date" />
+              </template>
+              <template #subtitle>
+                Date
+              </template>
+            </ListItem>
+          </v-col>
+          <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
+            <ListItem>
+              <template #icon>
+                <v-icon>$vuetify.icons.clock-1</v-icon>
+              </template>
+              <template #content>
+                <div>
+                  <div class="d-inline-block pr-8">
+                    <v-list-item-title class="text-p2 neutral--text text--darken-4">
+                      {{ match.startTime }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="text-p1 neutral--text text--darken-2">
+                      Start Time
+                    </v-list-item-subtitle>
+                  </div>
+                  <div v-if="match.finishTime" class="d-inline-block">
+                    <v-list-item-title class="text-p2 neutral--text text--darken-4">
+                      {{ match.finishTime }}
+                    </v-list-item-title>
+                    <v-list-item-subtitle class="text-p1 neutral--text text--darken-2">
+                      Finish Time
+                    </v-list-item-subtitle>
+                  </div>
                 </div>
-                <div v-if="match.finishTime" class="d-inline-block">
-                  <v-list-item-title class="text-p2 neutral--text text--darken-4">
-                    {{ match.finishTime }}
-                  </v-list-item-title>
-                  <v-list-item-subtitle class="text-p1 neutral--text text--darken-2">
-                    Finish Time
-                  </v-list-item-subtitle>
-                </div>
-              </div>
-            </template>
-          </ListItem>
-        </v-col>
-        <v-col cols="12" class="border-bottom pt-1 pb-0">
-          <ListItem>
-            <template #icon>
-              <v-icon>$vuetify.icons.user-1</v-icon>
-            </template>
-            <template #title>
-              {{ lead.firstname }} {{ lead.lastname }}
-            </template>
-            <template #subtitle>
-              Lead Staff Member
-            </template>
-          </ListItem>
-        </v-col>
-        <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
-          <ListItem>
-            <template #icon>
-              <v-icon>$vuetify.icons.location-1</v-icon>
-            </template>
-            <template #title>
-              <FxInHouseLocationLabel :match="match" />
-            </template>
-            <template #subtitle>
-              Location
-            </template>
-          </ListItem>
-        </v-col>
-        <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
-          <ListItem>
-            <template #icon>
-              <v-icon>$vuetify.icons.directory-outline</v-icon>
-            </template>
-            <template #title>
-              Further Information
-            </template>
-            <template #subtitle>
-              {{ event.info }}
-            </template>
-          </ListItem>
-        </v-col>
-      </v-row>
-    </v-container>
-  </FxEventItemCard>
+              </template>
+            </ListItem>
+          </v-col>
+          <v-col cols="12" class="border-bottom pt-1 pb-0">
+            <ListItem>
+              <template #icon>
+                <v-icon>$vuetify.icons.user-1</v-icon>
+              </template>
+              <template #title>
+                {{ lead.firstname }} {{ lead.lastname }}
+              </template>
+              <template #subtitle>
+                Lead Staff Member
+              </template>
+            </ListItem>
+          </v-col>
+          <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
+            <ListItem>
+              <template #icon>
+                <v-icon>$vuetify.icons.location-1</v-icon>
+              </template>
+              <template #title>
+                <FxInHouseLocationLabel :match="match" />
+              </template>
+              <template #subtitle>
+                Location
+              </template>
+            </ListItem>
+          </v-col>
+          <v-col cols="12" class="border-bottom border-right pt-1 pb-0">
+            <ListItem>
+              <template #icon>
+                <v-icon>$vuetify.icons.directory-outline</v-icon>
+              </template>
+              <template #title>
+                Further Information
+              </template>
+              <template #subtitle>
+                {{ event.info }}
+              </template>
+            </ListItem>
+          </v-col>
+        </v-row>
+      </v-container>
+    </FxEventItemCard>
+    <v-btn
+      v-if="canEditEvent && !formVisible"
+      height="50"
+      outlined
+      block
+      class="mt-4 hidden-md-and-up neutral--text text--darken-3"
+      @click="open"
+    >
+      <v-icon color="neutral darken-1">
+        $vuetify.icons.pen
+      </v-icon>Edit Details
+    </v-btn>
+  </div>
 </template>
 
 <script>
@@ -129,19 +143,20 @@ export default {
       lead: 'page/inHouseEvent/lead',
     }),
   },
-  created () {
-    this.formData = {
-      leadId: this.event.leadId,
-      info: this.event.info,
-      date: this.match.date,
-      startTime: this.match.startTime,
-      finishTime: this.match.finishTime,
-      location: this.match.location,
-      sportLocationId: this.match.sportLocationId,
-      otherLocation: this.match.otherLocation,
-    }
-  },
   methods: {
+    open () {
+      this.formData = {
+        leadId: this.event.leadId,
+        info: this.event.info || '',
+        date: this.match.date,
+        startTime: this.match.startTime,
+        finishTime: this.match.finishTime,
+        location: this.match.location,
+        sportLocationId: this.match.sportLocationId,
+        otherLocation: this.match.otherLocation,
+      }
+      this.formVisible = true
+    },
     async save () {
       this.loading = true
       const isValid = await this.$refs.form.validateAsync()
--- components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventResults.vue
+++ components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventResults.vue
@@ -1,35 +1,49 @@
 <template>
-  <FxEventItemCard v-if="canAddOrEditResult || hasResult">
-    <template #title>
-      Results
-    </template>
-    <template #actions>
-      <v-btn v-if="!formVisible && canAddOrEditResult" outlined @click="formVisible = true">
-        <v-icon small>
-          mdi-pencil-outline
-        </v-icon>Edit Result
-      </v-btn>
-    </template>
-    <v-container>
-      <v-row>
-        <v-col cols="12" class="pt-1 pb-0 px-0">
-          <FxInHouseEventResultForm
-            v-if="formVisible"
-            ref="form"
-            v-model="formData"
-            class="px-6"
-            :event-type="inHouseEvent.eventType"
-            :left-name="myTeam.name"
-            :right-name="opponentTeam.name"
-            :disabled="loading"
-            @submit="save"
-            @cancel="formVisible = false"
-          />
-          <FxInHouseEventExistingResult v-else class="rounded" />
-        </v-col>
-      </v-row>
-    </v-container>
-  </FxEventItemCard>
+  <div>
+    <FxEventItemCard v-if="canAddOrEditResult || hasResult">
+      <template #title>
+        Results
+      </template>
+      <template #actions>
+        <v-btn v-if="!formVisible && canAddOrEditResult" outlined @click="formVisible = true">
+          <v-icon small>
+            mdi-pencil-outline
+          </v-icon>Edit Result
+        </v-btn>
+      </template>
+      <v-container>
+        <v-row>
+          <v-col cols="12" class="pt-1 pb-0 px-0">
+            <FxInHouseEventResultForm
+              v-if="formVisible"
+              ref="form"
+              v-model="formData"
+              class="px-6"
+              :event-type="inHouseEvent.eventType"
+              :left-name="myTeam.name"
+              :right-name="opponentTeam.name"
+              :disabled="loading"
+              @submit="save"
+              @cancel="formVisible = false"
+            />
+            <FxInHouseEventExistingResult v-else class="rounded" />
+          </v-col>
+        </v-row>
+      </v-container>
+    </FxEventItemCard>
+    <v-btn
+      v-if="!formVisible && canAddOrEditResult"
+      outlined
+      block
+      height="50"
+      class="mt-4 hidden-md-and-up neutral--text text--darken-3"
+      @click="formVisible = true"
+    >
+      <v-icon color="neutral darken-1">
+        $vuetify.icons.pen
+      </v-icon>Edit Result
+    </v-btn>
+  </div>
 </template>
 
 <script>
