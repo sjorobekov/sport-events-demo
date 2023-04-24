f8324394b5e2a30eee648715919cdd0a157e4e77
Zhashtilek <zhashtilek.orozov@mavix.io>
2023-04-24 17:13:23 +0600

    feat(SOFTWARE-DEVELOPMENT-T-665): style in-house event score

--- components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventScoreBoardCard.vue
+++ components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventScoreBoardCard.vue
@@ -1,16 +1,20 @@
 <template>
-  <v-card class="rounded-t-0 score-card" height="152">
-    <v-row v-if="inHouseEvent.eventType === InHouseEventType.HOUSE_VS_HOUSE" no-gutters class="pa-6">
-      <v-col cols="4">
-        <FxTeam :color="myTeam.color" :logo="myTeam.logo" :name="myTeam.name" />
-      </v-col>
-      <v-col cols="4" class="text-center">
-        <FxInHouseEventResultCenterBoard />
-      </v-col>
-      <v-col cols="4">
-        <FxTeam class="pl-0" :name="opponentTeam.name" :color="opponentTeam.color" :logo="opponentTeam.logo" />
-      </v-col>
-    </v-row>
+  <v-card class="rounded-t-0 score-card">
+    <table v-if="inHouseEvent.eventType === InHouseEventType.HOUSE_VS_HOUSE" style="width: 100%" class="pa-5">
+      <tbody>
+        <tr class="hover-none">
+          <td class="text-center team-cell pb-2 pb-md-3">
+            <FxTeam :color="myTeam.color" :logo="myTeam.logo" :name="myTeam.name" />
+          </td>
+          <td rowspan="2">
+            <FxInHouseEventResultCenterBoard />
+          </td>
+          <td class="text-center team-cell pb-2 pb-md-3">
+            <FxTeam class="pl-0" :name="opponentTeam.name" :color="opponentTeam.color" :logo="opponentTeam.logo" />
+          </td>
+        </tr>
+      </tbody>
+    </table>
 
     <div v-else class="py-6 d-flex gap-md justify-center">
       <v-slide-group
@@ -64,8 +68,19 @@ export default {
 </script>
 <style scoped lang="sass">
 @import '~vuetify/src/styles/styles.sass'
+.team-cell
+  width: 40%
+.score-card
+  border-top-left-radius: 0!important
+  border-top-right-radius: 0!important
+  border-bottom-left-radius: 12px!important
+  border-bottom-right-radius: 12px!important
+
+.hover-none:hover
+  background-color: white!important
 
 @media #{map-get($display-breakpoints, 'sm-and-down')}
+
   .score-card.v-card
     border-radius: 0!important
     border: 0!important
--- components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventTeam.vue
+++ components/PageComponents/FxInHouseEventIndividualPage/FxInHouseEventTeam.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="text-center">
     <FxSchoolLogo class="mx-auto" size="64" :value="logo" :alt="name" :color="color" />
-    <div class="text-h5 neutral--text text--darken-4">
+    <div class="text-center team-name px-2 px-md-6 neutral--text text--darken-4">
       {{ name }}
     </div>
   </div>
@@ -29,3 +29,16 @@ export default {
   },
 }
 </script>
+
+<style scoped lang="sass">
+@import '~vuetify/src/styles/styles.sass'
+.team-name
+  font-size: 1.4375rem
+  line-height: 1.3rem
+  font-family: ProximaNova-Semibold, sans-serif
+
+@media #{map-get($display-breakpoints, 'sm-and-down')}
+  .team-name
+    font-size: 0.975rem
+    line-height: 1rem
+</style>
